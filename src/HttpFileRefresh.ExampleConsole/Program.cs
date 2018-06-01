6339ad80ca779f1c65a0777f74e657f1ddc294e6
David Hillier <david.hillier@guestline.com>
2018-06-01 23:21:28 +0100

    Simplified example

--- src/HttpFileRefresh.ExampleConsole/Program.cs
+++ src/HttpFileRefresh.ExampleConsole/Program.cs
@@ -1,56 +1,45 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.IO;
 using Microsoft.Extensions.Logging;
 
 namespace HttpFileRefresh.ExampleConsole
 {
-    class Program
+  class Program
     {
         static IConfiguration Configuration { get; set; }
 
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var env = "development";
+            // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/generic-host?view=aspnetcore-2.1
+            var host = new HostBuilder()
+            .ConfigureAppConfiguration((context, builder) => {
+                builder.AddEnvironmentVariables();
+                builder.AddJsonFile("appsettings.json", optional: true);
+                builder.AddJsonFile(
+                    $"appsettings.{context.HostingEnvironment.EnvironmentName}.json", 
+                    optional: true);
+                builder.AddCommandLine(args);
+            })
+            .ConfigureLogging((context, builder) => {
+                builder.AddConsole();
+                builder.AddDebug();
+            })
+            .ConfigureServices((context, services) => {
+                services.Configure<AadAuthOptions>(Configuration.GetSection("auth"));
+                services.AddSingleton<AadAuthorizationHandler>();
+                services.AddHttpClient("filerefresh").AddHttpMessageHandler<AadAuthorizationHandler>();
 
-            Configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables()
-                .Build();
-        
-            var services = new ServiceCollection();
-            services.AddSingleton<IConfiguration>(Configuration);
-            services.AddLogging(configure => {
-                configure.AddConsole();
-                configure.AddConfiguration(Configuration.GetSection("Logging"));
-            });
+                services.Configure<FileRefreshServiceOptions>(Configuration.GetSection("filerefresh"));
+                services.AddSingleton<IHostedService, FileRefreshService>();
+            })
+            .Build();
 
-            services.AddSingleton<HostedServiceExecutor>();
-            
-            ConfigureServices(services);
-
-            var serviceProvider = services.BuildServiceProvider();             
-            var hostedServiceExecutor = serviceProvider.GetRequiredService<HostedServiceExecutor>();
-
-            hostedServiceExecutor.StartAsync(CancellationToken.None).GetAwaiter().GetResult();
-
-        
-        }
-        public static void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<AadAuthOptions>(Configuration.GetSection("auth"));
-            services.AddSingleton<AadAuthorizationHandler>();
-            services.AddHttpClient("filerefresh").AddHttpMessageHandler<AadAuthorizationHandler>();
-
-            services.Configure<FileRefreshServiceOptions>(Configuration.GetSection("filerefresh"));
-            services.AddSingleton<IHostedService, FileRefreshService>();
+            await host.RunAsync();
         }
     }
 }
